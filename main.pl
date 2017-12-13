ab2453e8dd4c2c83d8980531670803bdbbc3e836
gromoff97 <gromoff97@mail.ru>
2017-12-13 12:23:21 +0300

    scanning and checking part of code was moved beyond the loop

--- main.pl
+++ main.pl
@@ -14,27 +14,27 @@ my $input_unit;
 my $output_unit;
 my $multiplier;
 
+print("Print Input unit name\n");
+$input_unit = <STDIN>;
+
+print("Print Output unit name\n");
+$output_unit = <STDIN>;
+
+print('Print how many ',substr($output_unit, 0, -1), ' are now in one ', $input_unit);
+$multiplier = <STDIN>;
+
+unless ($input_unit =~ m/^[a-z]+$/ && $output_unit =~ m/^[a-z]+$/ && $multiplier =~ m/^\d+(\.\d+)?$/)
+{
+	print("bad unit/multiplier name. Closing\n");
+	exit 2;
+}
+
 foreach $file_name (@ARGV)
 {
 	open(INPUTFILE,'<',$file_name) or die "can't open $file_name : $!\n";
 		open(OUTPUTFILE,'>','D'."$file_name") or die "can't open $file_name : $!\n";
-			
-			print("Print Input unit name\n");
-			$input_unit = <STDIN>;
-
-			print("Print Output unit name\n");
-			$output_unit = <STDIN>;
-
-			print('Print how many ',substr($output_unit, 0, -1), ' are in one ', $input_unit);
-			$multiplier = <STDIN>;
-
-			unless ($input_unit =~ m/^[a-z]+$/ && $output_unit =~ m/^[a-z]+$/ && $multiplier =~ m/^\d+(\.\d+)?$/)
-			{
-				print("bad unit/multiplier name. Closing\n");
-				close OUTPUTFILE;
-				close INPUTFILE;
-				exit 2;
-			}
+		
+		#code
 
 		close OUTPUTFILE;
 	close INPUTFILE;
