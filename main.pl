96e263bff1ebfb1dd310f839018f0cf2da9dad7b
gromoff97 <gromoff97@mail.ru>
2017-12-13 11:11:42 +0300

    simple argchecking and main loop were created

--- main.pl
+++ main.pl
@@ -1,2 +1,23 @@
 use strict;
-use warnings;	
+use warnings;
+
+if (($#ARGV + 1) == 0)
+{
+	print "Usage: perl main.pl <file_name1> <file_name2> ... \n";
+	exit 1;
+}
+
+my $file_name;
+
+foreach $file_name (@ARGV)
+{
+	open(INPUTFILE,'<',$file_name) or die "can't open $file_name : $!";
+		open(OUTPUTFILE,'>','D'."$file_name") or die "can't open $file_name : $!";
+
+
+		#code
+
+
+		close OUTPUTFILE;
+	close INPUTFILE;
+}
